9e8ecf8d89331a10f95585ac13f0a3b945063679
Fr13dr1ch <friedrichlucas.99@gmail.com>
2020-04-05 21:31:36 +0200

    Optimize installation scripts

--- install.sh
+++ install.sh
@@ -1,2 +1,18 @@
 # Run all installation scripts
 # TODO: Variable deklaration forms
+
+
+echo "Update the server..."
+sudo apt-get update -y > 2>&1 >/dev/null
+sudo apt-get upgrade -y > 2>&1 >/dev/null
+
+echo "Install the snap demon..."
+sudo apt install snapd -y > 2>&1 >/dev/null
+
+echo "Install kubernetes..."
+sudo snap install microk8s --channel=1.18 --classic > 2>&1 >/dev/null
+
+echo "Setup node configuration..."
+bash ./node/install.sh
+
+echo "Install kubernetes services,,,"
