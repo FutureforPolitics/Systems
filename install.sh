5d9349caf89d676ef1ecdb162227e18b3d78c168
Fr13dr1ch <friedrichlucas.99@gmail.com>
2020-04-05 21:39:55 +0200

    Optimize Configuration and installation script

--- install.sh
+++ install.sh
@@ -6,6 +6,9 @@ echo "Update the server..."
 sudo apt-get update -y 2>&1 >/dev/null
 sudo apt-get upgrade -y 2>&1 >/dev/null
 
+echo "Install the system utils..."
+sudo apt install vim -y 2>&1 >/dev/null
+
 echo "Install the snap demon..."
 sudo apt install snapd -y 2>&1 >/dev/null
 
--- node/install.sh
+++ node/install.sh
@@ -1,4 +1,4 @@
 
-cp ./etc/.bashrc ~/.bashrc
-cp ./etc/.vimrc ~/.vimrc
-cp ./etc/motd /etc/motd
+cp ./node/etc/.bashrc ~/.bashrc
+cp ./node/etc/.vimrc ~/.vimrc
+cp ./node/etc/motd /etc/motd
