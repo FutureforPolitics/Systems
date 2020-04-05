6c162fbcd1b159f484a8e024efc490aa89f86849
Fr13dr1ch <friedrichlucas.99@gmail.com>
2020-04-05 21:33:18 +0200

    Fix Bugs

--- install.sh
+++ install.sh
@@ -3,14 +3,14 @@
 
 
 echo "Update the server..."
-sudo apt-get update -y > 2>&1 >/dev/null
-sudo apt-get upgrade -y > 2>&1 >/dev/null
+sudo apt-get update -y 2>&1 >/dev/null
+sudo apt-get upgrade -y 2>&1 >/dev/null
 
 echo "Install the snap demon..."
-sudo apt install snapd -y > 2>&1 >/dev/null
+sudo apt install snapd -y 2>&1 >/dev/null
 
 echo "Install kubernetes..."
-sudo snap install microk8s --channel=1.18 --classic > 2>&1 >/dev/null
+sudo snap install microk8s --channel=1.18 --classic 2>&1 >/dev/null
 
 echo "Setup node configuration..."
 bash ./node/install.sh
